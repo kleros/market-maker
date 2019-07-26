a9f0cee7090e61c1c42eb699a51a3f952c4404c5
Ferit Tunçer <ferit@cryptolab.net>
2019-07-26 21:15:29 +0300

    refactor(ethfinex-market-maker): increase order interval to 0.00075

--- src/ethfinex-market-maker.js
+++ src/ethfinex-market-maker.js
@@ -11,7 +11,7 @@ const ETHFINEX_WEBSOCKET_API = 'wss://api.ethfinex.com/ws/2/'
 BigNumber.config({ EXPONENTIAL_AT: [-30, 40] })
 
 const SYMBOL = 'tPNKETH'
-const ORDER_INTERVAL = new BigNumber(0.00025)
+const ORDER_INTERVAL = new BigNumber(0.00075)
 const MIN_ETH_SIZE = new BigNumber(0.15)
 
 function sleep(ms) {
