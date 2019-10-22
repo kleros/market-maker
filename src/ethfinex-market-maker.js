66ddeb24a869e88d68e57598e00fff0725d53b30
Ferit Tunçer <ferit@cryptolab.net>
2019-10-22 19:02:50 +0300

    refactor(ethfinex-market-maker): change ws endpoint

--- src/ethfinex-market-maker.js
+++ src/ethfinex-market-maker.js
@@ -1,23 +1,23 @@
-const assert = require('assert')
-const crypto = require('crypto')
-const program = require('commander')
-const WS = require('ws')
-const BigNumber = require('bignumber.js')
-const ethfinexRestWrapper = require('./ethfinex-rest-api-wrapper')
-const { chunk } = require('lodash')
-const calculateMaximumReserve = require('./utils').calculateMaximumReserve
-const utils = require('./utils')
-const Mutex = require('async-mutex').Mutex
-const fs = require('fs')
+const assert = require("assert");
+const crypto = require("crypto");
+const program = require("commander");
+const WS = require("ws");
+const BigNumber = require("bignumber.js");
+const ethfinexRestWrapper = require("./ethfinex-rest-api-wrapper");
+const { chunk } = require("lodash");
+const calculateMaximumReserve = require("./utils").calculateMaximumReserve;
+const utils = require("./utils");
+const Mutex = require("async-mutex").Mutex;
+const fs = require("fs");
 
-const ETHFINEX_WEBSOCKET_API = 'wss://api.ethfinex.com/ws/2/'
+const ETHFINEX_WEBSOCKET_API = "wss://api.bitfinex.com/ws/2/";
 
-BigNumber.config({ EXPONENTIAL_AT: [-30, 40] })
+BigNumber.config({ EXPONENTIAL_AT: [-30, 40] });
 
-const SYMBOL = 'tPNKETH'
-const ORDER_INTERVAL = new BigNumber(0.0005)
-const MIN_ETH_SIZE = new BigNumber(0.03)
-const WEBSOCKET_CONNECTION_DOWN = 123
+const SYMBOL = "tPNKETH";
+const ORDER_INTERVAL = new BigNumber(0.0005);
+const MIN_ETH_SIZE = new BigNumber(0.03);
+const WEBSOCKET_CONNECTION_DOWN = 123;
 
 module.exports = {
   getOrders: function(steps, sizeInEther, reserve) {
@@ -25,27 +25,27 @@ module.exports = {
       steps,
       sizeInEther,
       reserve
-    )
+    );
     const newExchangeLimitOrder = (amount, price) => [
-      'on',
+      "on",
       {
         amount,
         cid: Math.floor(Math.random() * 2 ** 45),
         price,
         symbol: SYMBOL,
-        type: 'EXCHANGE LIMIT'
+        type: "EXCHANGE LIMIT"
       }
-    ]
+    ];
 
-    const orders = []
-    const equilibrium = reserve.eth.div(reserve.pnk)
+    const orders = [];
+    const equilibrium = reserve.eth.div(reserve.pnk);
     for (let i = 0; i < rawOrders.length; i++) {
-      const orderAmount = rawOrders[i].pnk
-      const orderPrice = rawOrders[i].eth.div(rawOrders[i].pnk).absoluteValue()
+      const orderAmount = rawOrders[i].pnk;
+      const orderPrice = rawOrders[i].eth.div(rawOrders[i].pnk).absoluteValue();
 
       if (orderAmount.isPositive())
-        assert(orderPrice.lt(equilibrium), orderPrice.toString())
-      else assert(orderPrice.gt(equilibrium), orderPrice.toString())
+        assert(orderPrice.lt(equilibrium), orderPrice.toString());
+      else assert(orderPrice.gt(equilibrium), orderPrice.toString());
 
       orders.push(
         newExchangeLimitOrder(
@@ -55,83 +55,83 @@ module.exports = {
             .absoluteValue()
             .toString()
         )
-      )
+      );
     }
-    const chunks = chunk(orders, 15).map(c => [0, 'ox_multi', null, c])
-    return chunks
+    const chunks = chunk(orders, 15).map(c => [0, "ox_multi", null, c]);
+    return chunks;
   },
 
   autoMarketMake: async steps => {
-    const date = new Date()
-    const mutex = new Mutex()
-    let flag = 0
+    const date = new Date();
+    const mutex = new Mutex();
+    let flag = 0;
 
-    assert(steps <= 128, 'You exceeded Ethfinex maximum order limit.')
-    let initialOrdersPlaced = false
+    assert(steps <= 128, "You exceeded Ethfinex maximum order limit.");
+    let initialOrdersPlaced = false;
 
-    const w = new WS(ETHFINEX_WEBSOCKET_API)
+    const w = new WS(ETHFINEX_WEBSOCKET_API);
     const CANCEL_ALL_ORDERS = JSON.stringify([
       0,
-      'oc_multi',
+      "oc_multi",
       null,
       {
         all: 1
       }
-    ])
-    let highestBid
-    let lowestAsk
-    let orders
-    let reserve
-    let availableETH
-    let availablePNK
+    ]);
+    let highestBid;
+    let lowestAsk;
+    let orders;
+    let reserve;
+    let availableETH;
+    let availablePNK;
 
-    fs.readFile('ethfinex_reserve.txt', 'utf-8', (err, data) => {
-      if (err) return
-      reserve = JSON.parse(data)
-      reserve.pnk = new BigNumber(reserve.pnk)
-      reserve.eth = new BigNumber(reserve.eth)
-      console.log('Found a reserve file, loading...')
-    })
+    fs.readFile("ethfinex_reserve.txt", "utf-8", (err, data) => {
+      if (err) return;
+      reserve = JSON.parse(data);
+      reserve.pnk = new BigNumber(reserve.pnk);
+      reserve.eth = new BigNumber(reserve.eth);
+      console.log("Found a reserve file, loading...");
+    });
 
     if (
-      typeof process.env.ETHFINEX_KEY === 'undefined' ||
-      typeof process.env.ETHFINEX_SECRET === 'undefined'
+      typeof process.env.ETHFINEX_KEY === "undefined" ||
+      typeof process.env.ETHFINEX_SECRET === "undefined"
     ) {
       console.error(
-        'Please export ETHFINEX_KEY and ETHFINEX_SECRET environment variables.'
-      )
-      process.exit(2)
+        "Please export ETHFINEX_KEY and ETHFINEX_SECRET environment variables."
+      );
+      process.exit(2);
     }
     heartbeat = client => {
-      clearTimeout(client.pingTimeout)
+      clearTimeout(client.pingTimeout);
       client.pingTimeout = setTimeout(function() {
-        process.exit(utils.WEBSOCKET_CONNECTION_DOWN)
-      }, 60000)
-    }
-    w.on('open', () => {
-      heartbeat(w)
-      w.send(authenticationPayload())
-    })
+        process.exit(utils.WEBSOCKET_CONNECTION_DOWN);
+      }, 60000);
+    };
+    w.on("open", () => {
+      heartbeat(w);
+      w.send(authenticationPayload());
+    });
 
-    w.on('error', async event => {
-      console.error(event)
-      await new Promise(resolve => setTimeout(resolve, 30000))
-      module.exports.autoMarketMake(steps)
-    })
+    w.on("error", async event => {
+      console.error(event);
+      await new Promise(resolve => setTimeout(resolve, 30000));
+      module.exports.autoMarketMake(steps);
+    });
 
-    w.on('close', function clear() {
-      clearTimeout(this.pingTimeout)
-    })
+    w.on("close", function clear() {
+      clearTimeout(this.pingTimeout);
+    });
 
-    w.on('message', async msg => {
-      const parsed = JSON.parse(msg)
+    w.on("message", async msg => {
+      const parsed = JSON.parse(msg);
 
       if (
-        parsed[1] != 'on' &&
-        parsed[1] != 'n' &&
-        parsed[1] != 'oc' &&
-        parsed[1] != 'hb' &&
-        parsed[1] != 'bu'
+        parsed[1] != "on" &&
+        parsed[1] != "n" &&
+        parsed[1] != "oc" &&
+        parsed[1] != "hb" &&
+        parsed[1] != "bu"
       ) {
         if (reserve) {
           console.log(
@@ -140,20 +140,20 @@ module.exports = {
             )} ETH: ${reserve.eth} | PNK: ${
               reserve.pnk
             } | ETH/PNK: ${reserve.eth.div(reserve.pnk)}`
-          )
+          );
         }
-        console.log(parsed)
+        console.log(parsed);
       } else {
-        heartbeat(w)
+        heartbeat(w);
       }
 
-      if (Array.isArray(parsed) && parsed[1] == 'wu') {
-        const payload = parsed[2]
-        if (payload[1] == 'PNK') {
-          availablePNK = new BigNumber(payload[2])
-        } else if (payload[1] == 'ETH') {
-          availableETH = new BigNumber(payload[2])
-        } else console.log('Unhandled wallet update.')
+      if (Array.isArray(parsed) && parsed[1] == "wu") {
+        const payload = parsed[2];
+        if (payload[1] == "PNK") {
+          availablePNK = new BigNumber(payload[2]);
+        } else if (payload[1] == "ETH") {
+          availableETH = new BigNumber(payload[2]);
+        } else console.log("Unhandled wallet update.");
       }
 
       if (!reserve && availableETH && availablePNK && lowestAsk && highestBid) {
@@ -161,9 +161,9 @@ module.exports = {
           availableETH,
           availablePNK,
           highestBid.plus(lowestAsk).div(2)
-        )
+        );
 
-        const date = new Date()
+        const date = new Date();
 
         console.log(
           `${date.getDate()}:${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} # RESERVE <> ETH*PNK: ${reserve.eth.times(
@@ -171,12 +171,12 @@ module.exports = {
           )} ETH: ${reserve.eth} | PNK: ${
             reserve.pnk
           } | ETH/PNK: ${reserve.eth.div(reserve.pnk)}`
-        )
+        );
 
-        fs.writeFile('ethfinex_reserve.txt', JSON.stringify(reserve), err => {
-          if (err) console.log(err)
-          console.log('Reserve saved to file.')
-        })
+        fs.writeFile("ethfinex_reserve.txt", JSON.stringify(reserve), err => {
+          if (err) console.log(err);
+          console.log("Reserve saved to file.");
+        });
       }
 
       if (reserve && !initialOrdersPlaced) {
@@ -184,44 +184,44 @@ module.exports = {
           parseInt(steps),
           MIN_ETH_SIZE,
           reserve
-        )
+        );
 
-        console.log('Placing orders...')
-        for (batch of orders) w.send(JSON.stringify(batch))
-        initialOrdersPlaced = true
+        console.log("Placing orders...");
+        for (batch of orders) w.send(JSON.stringify(batch));
+        initialOrdersPlaced = true;
       }
 
-      if (parsed.event == 'info') {
-        const ticker = await ethfinexRestWrapper.getTicker()
-        console.log(ticker)
-        highestBid = new BigNumber(ticker[0])
-        lowestAsk = new BigNumber(ticker[2])
+      if (parsed.event == "info") {
+        const ticker = await ethfinexRestWrapper.getTicker();
+        console.log(ticker);
+        highestBid = new BigNumber(ticker[0]);
+        lowestAsk = new BigNumber(ticker[2]);
       }
 
-      if (parsed.event == 'auth') {
+      if (parsed.event == "auth") {
       }
 
       if (
         Array.isArray(parsed) &&
-        parsed[1] == 'te' &&
+        parsed[1] == "te" &&
         parsed[2][1] == SYMBOL
       ) {
-        const release = await mutex.acquire()
-        console.log('Cancelling orders...')
-        w.send(CANCEL_ALL_ORDERS)
+        const release = await mutex.acquire();
+        console.log("Cancelling orders...");
+        w.send(CANCEL_ALL_ORDERS);
 
-        const tradeExecutionLog = parsed[2]
-        const pinakionAmount = new BigNumber(tradeExecutionLog[4])
-        const price = new BigNumber(tradeExecutionLog[5])
+        const tradeExecutionLog = parsed[2];
+        const pinakionAmount = new BigNumber(tradeExecutionLog[4]);
+        const price = new BigNumber(tradeExecutionLog[5]);
 
-        const oldInvariant = reserve.eth.times(reserve.pnk)
+        const oldInvariant = reserve.eth.times(reserve.pnk);
 
         const etherAmount = pinakionAmount
           .times(price)
-          .times(new BigNumber('-1'))
+          .times(new BigNumber("-1"));
 
-        reserve.eth = reserve.eth.plus(etherAmount)
-        reserve.pnk = reserve.pnk.plus(pinakionAmount)
+        reserve.eth = reserve.eth.plus(etherAmount);
+        reserve.pnk = reserve.pnk.plus(pinakionAmount);
 
         console.log(
           `${date.getDate()}:${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} # RESERVE <> ETH*PNK: ${reserve.eth.times(
@@ -229,45 +229,45 @@ module.exports = {
           )} ETH: ${reserve.eth} | PNK: ${
             reserve.pnk
           } | ETH/PNK: ${reserve.eth.div(reserve.pnk)}`
-        )
+        );
 
-        const TOLERANCE = 0.99999
-        const newInvariant = reserve.eth.times(reserve.pnk)
+        const TOLERANCE = 0.99999;
+        const newInvariant = reserve.eth.times(reserve.pnk);
         assert(
           newInvariant.gte(oldInvariant.times(TOLERANCE)),
           `New Invariant: ${newInvariant}  Old Invariant: ${oldInvariant}\nInvariant should not decrease. Check bounding curve implemention.`
-        )
+        );
 
-        fs.writeFile('ethfinex_reserve.txt', JSON.stringify(reserve), err => {
-          if (err) console.log(err)
-          console.log('Reserve saved to file.')
-        })
+        fs.writeFile("ethfinex_reserve.txt", JSON.stringify(reserve), err => {
+          if (err) console.log(err);
+          console.log("Reserve saved to file.");
+        });
 
         const orders = module.exports.getOrders(
           parseInt(steps),
           MIN_ETH_SIZE,
           reserve
-        )
-        console.log('Placing orders...')
+        );
+        console.log("Placing orders...");
 
-        for (batch of orders) w.send(JSON.stringify(batch))
-        await new Promise(resolve => setTimeout(resolve, 2000))
+        for (batch of orders) w.send(JSON.stringify(batch));
+        await new Promise(resolve => setTimeout(resolve, 2000));
 
-        release()
-        flag++
+        release();
+        flag++;
         if (flag > 20) {
-          console.log('Kill switch...')
-          process.exit(5)
+          console.log("Kill switch...");
+          process.exit(5);
         }
       }
-    })
+    });
     const authenticationPayload = function() {
-      const nonce = Date.now() * 1000
-      const payload = `AUTH${nonce}`
+      const nonce = Date.now() * 1000;
+      const payload = `AUTH${nonce}`;
       const signature = crypto
-        .createHmac('SHA384', process.env.ETHFINEX_SECRET)
+        .createHmac("SHA384", process.env.ETHFINEX_SECRET)
         .update(payload)
-        .digest('hex')
+        .digest("hex");
 
       return JSON.stringify({
         apiKey: process.env.ETHFINEX_KEY,
@@ -275,8 +275,8 @@ module.exports = {
         authPayload: payload,
         authSig: signature,
         dms: 4,
-        event: 'auth'
-      })
-    }
+        event: "auth"
+      });
+    };
   }
-}
+};
