b3c6dab1d09130ba463397131dac6040a5c8404e
Ferit Tunçer <ferit@cryptolab.net>
2020-01-31 00:53:25 +0300

    fix(ethfinex-rest-api-wrapper): pass empty object to body parameter

--- src/ethfinex-rest-api-wrapper.js
+++ src/ethfinex-rest-api-wrapper.js
@@ -1,60 +1,63 @@
-const AUTHENTICATED = 'https://api.bitfinex.com'
-const PUBLIC = 'https://api-pub.bitfinex.com'
+const AUTHENTICATED = "https://api.bitfinex.com";
+const PUBLIC = "https://api-pub.bitfinex.com";
 
-const fetch = require('node-fetch')
-const crypto = require('crypto')
+const fetch = require("node-fetch");
+const crypto = require("crypto");
 
 module.exports = {
   getSignatureHash: function(apiPath, body, nonce) {
-    const signature = `/api/${apiPath}${nonce}${JSON.stringify(body)}`
+    const signature = `/api/${apiPath}${nonce}${JSON.stringify(body)}`;
     const sig = crypto
-      .createHmac('SHA384', process.env.ETHFINEX_SECRET)
-      .update(signature)
-    return (shex = sig.digest('hex'))
+      .createHmac("SHA384", process.env.ETHFINEX_SECRET)
+      .update(signature);
+    return (shex = sig.digest("hex"));
   },
 
-  ticker: async function(symbol = 'tPNKETH') {
-    return await fetch(`${PUBLIC}/v2/ticker/${symbol}`).then(
-      function(response) {
-        return response.json()
-      }
-    )
+  ticker: async function(symbol = "tPNKETH") {
+    return await fetch(`${PUBLIC}/v2/ticker/${symbol}`).then(function(
+      response
+    ) {
+      return response.json();
+    });
   },
-  
+
   wallets: async function(nonce) {
-    return await fetch(
-      `${AUTHENTICATED}/v2/auth/r/wallets?type=price`,
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          'bfx-nonce': nonce,
-          'bfx-apikey': process.env.ETHFINEX_KEY,
-          'bfx-signature': module.exports.getSignatureHash("v2/auth/r/wallets", {}, nonce)
-        },
-        json: true,
-        method: 'POST',
-        body: JSON.stringify(body)
-      }
-    ).then(function(response) {
-      return response.json()
-    })
-  },
-
-  orders: async function(nonce, symbol = 'tPNKETH') {
-    return await fetch(`${AUTHENTICATED}/v2/auth/r/orders/${symbol}`, {
+    return await fetch(`${AUTHENTICATED}/v2/auth/r/wallets?type=price`, {
       headers: {
-        'Content-Type': 'application/json',
-        'bfx-nonce': nonce,
-        'bfx-apikey': process.env.ETHFINEX_KEY,
-        'bfx-signature': module.exports.getSignatureHash("v2/auth/r/orders", {}, nonce)
+        "Content-Type": "application/json",
+        "bfx-nonce": nonce,
+        "bfx-apikey": process.env.ETHFINEX_KEY,
+        "bfx-signature": module.exports.getSignatureHash(
+          "v2/auth/r/wallets",
+          {},
+          nonce
+        )
       },
       json: true,
-      method: 'POST',
+      method: "POST",
       body: JSON.stringify({})
-    }).then(
-      function(response) {
-        return response.json()
-      }
-    )
+    }).then(function(response) {
+      return response.json();
+    });
+  },
+
+  orders: async function(nonce, symbol = "tPNKETH") {
+    return await fetch(`${AUTHENTICATED}/v2/auth/r/orders/${symbol}`, {
+      headers: {
+        "Content-Type": "application/json",
+        "bfx-nonce": nonce,
+        "bfx-apikey": process.env.ETHFINEX_KEY,
+        "bfx-signature": module.exports.getSignatureHash(
+          "v2/auth/r/orders",
+          {},
+          nonce
+        )
+      },
+      json: true,
+      method: "POST",
+      body: JSON.stringify({})
+    }).then(function(response) {
+      return response.json();
+    });
   }
-}
+};
