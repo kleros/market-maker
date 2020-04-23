c3a4b9d5aff74253d4bb7170dbaef352b5c26ed7
Ferit Tunçer <ferit@cryptolab.net>
2020-04-23 14:26:42 +0300

    refactor(efx): kill on 1000 trades instead of 200 trades

--- src/ethfinex-market-maker.js
+++ src/ethfinex-market-maker.js
@@ -344,7 +344,7 @@ module.exports = {
         if (!filledPartially) {
           noOfTrades++
           console.log(`Number of trades done: ${noOfTrades}`)
-          if (noOfTrades > 200) process.exit(utils.ExitCodes.DONT_RESTART)
+          if (noOfTrades > 1000) process.exit(utils.ExitCodes.DONT_RESTART)
 
           console.log(
             `${new Date().toISOString()} # tu | Order filled fully. Cancelling orders in order to replace all...`
